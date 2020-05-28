2693d845b3f532ace2a22314c45ecf328aadbe4f
Pedro Carrera <procarrera@gmail.com>
2020-05-28 16:54:01 -0300

    feat: add input robot and state

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
 const robots = {
-  userInput: require("./robots/user-input.js"),
+  input: require("./robots/input.js"),
   text: require("./robots/text.js"),
+  state: require("./robots/state"),
 };
 
 async function start() {
-  const content = {
-    maximumSentences: 7,
-  };
+  robots.input();
+  await robots.text();
 
-  await robots.userInput(content);
-  await robots.text(content);
-  console.log(JSON.stringify(content, null, 4));
+  const content = robots.state.load();
+  console.dir(content, { depth: null });
 }
 start();
