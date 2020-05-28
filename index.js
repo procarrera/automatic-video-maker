e0333885cd48273066376a18d59f3989a5ed5b68
Pedro Carrera <procarrera@gmail.com>
2020-05-28 18:51:34 -0300

    feat: add google images search

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const robots = {
-  input: require("./robots/input.js"),
-  text: require("./robots/text.js"),
+  input: require("./robots/input"),
+  text: require("./robots/text"),
   state: require("./robots/state"),
+  image: require("./robots/images"),
 };
 
 async function start() {
-  robots.input();
-  await robots.text();
+ // robots.input();
+ // await robots.text();
+  await robots.image();
 
   const content = robots.state.load();
   console.dir(content, { depth: null });
